ae4b9982e292a495989e36dbb40aa7a3c5131ae1
Bas Buermans <bas@klaassenontwerp.nl>
2023-03-07 10:00:27 +0100

    Updates site path in Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bazcodev.github.io',
+    site: 'https://www.olivier.software',
 });
